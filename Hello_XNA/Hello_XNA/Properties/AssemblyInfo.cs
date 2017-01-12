d2a59bdc9eaf3fcf9cbf7c46d82f88de50a19082
j_hogi.hwang <j_hogi.hwang@DO-J-HOGI-HWA03>
2017-01-12 12:44:22 +0900

    Change Title name

--- Hello_XNA/Hello_XNA/Properties/AssemblyInfo.cs
+++ Hello_XNA/Hello_XNA/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello_XNA")]
+[assembly: AssemblyTitle("Go Zaku!")]
 [assembly: AssemblyProduct("Hello_XNA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
